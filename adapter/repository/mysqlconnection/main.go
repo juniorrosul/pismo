dcddadc0b328a00739cf48faef5e56e95798fc63
Madson Verdi Junior <junior2590@gmail.com>
2020-10-06 08:31:45 -0300

    Add mysql to environment.

--- adapter/repository/mysqlconnection/main.go
+++ adapter/repository/mysqlconnection/main.go
@@ -7,6 +7,14 @@ import (
 	"gorm.io/gorm"
 )
 
+const (
+	dbName     = "pismo"
+	dbHost     = "localhost"
+	dbUser     = "madsonjr"
+	dbPassword = "pismo"
+	dbPort     = "3306"
+)
+
 func connect() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
